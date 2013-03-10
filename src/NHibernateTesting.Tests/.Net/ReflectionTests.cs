6a39d7a964116afe0c6b8ed27198ec5b5a15cc66
Rodrigo Silva de Andrade <dangling@gmail.com>
2013-03-10 14:47:12 -0300

    retreving classmaps from nexted types

---
+++ src/NHibernateTesting.Tests/.Net/ReflectionTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using FluentNHibernate.Mapping;
+using NUnit.Framework;
+using System.Linq;
+
+namespace NHibernateTesting.Tests.Net
+{
+    public class ReflectionTests
+    {
+        public class Foo
+        {
+            public class Bar { }
+            public class Fee { }
+        }
+
+        [Test]
+        public void CanRetrieveNextedClasses()
+        {
+            var nextedTypes = typeof(Foo).GetNestedTypes();
+            nextedTypes.ShouldBeEquivalentTo(new object[] { typeof(Foo.Bar), typeof(Foo.Fee) });
+        }
+
+        public class FooMap : ClassMap<Foo>
+        {
+        }
+
+        public class BarMap : ClassMap<Foo.Bar>
+        {
+        }
+
+        [Test]
+        public void CanRetrieveAllNextedClassesThatDerivesFromClassMap()
+        {
+            var classMaps = from type in typeof (ReflectionTests).GetNestedTypes()
+                            where type.BaseType != null
+                            let baseType = type.BaseType
+                            where baseType.IsGenericType
+                            where baseType.GetGenericTypeDefinition().IsAssignableFrom(typeof (ClassMap<>))
+                            select type;
+
+            classMaps.ShouldBeEquivalentTo(new object[] { typeof(FooMap), typeof(BarMap) });
+        }
+    }
+}
