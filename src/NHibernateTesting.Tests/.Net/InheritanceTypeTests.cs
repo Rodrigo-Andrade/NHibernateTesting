f500fcad73e697c1c4723a20591707eefd83c608
Rodrigo Silva de Andrade <dangling@gmail.com>
2013-03-10 15:00:46 -0300

    confirming how i imagine get type works on derived classes

---
+++ src/NHibernateTesting.Tests/.Net/InheritanceTypeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace NHibernateTesting.Tests.Net
+{
+    public class InheritanceTypeTests
+    {
+        public class Foo
+        {
+            public virtual Type ReturnType()
+            {
+                return GetType();
+            }
+        }
+
+        public class Bar : Foo{}
+
+        [Test]
+        public void DerivedTypeShouldReturnTypeOfDerivedType()
+        {
+            var bar = new Bar();
+
+            bar.ReturnType().Should().Be(typeof(Bar));
+        }
+    }
+}
