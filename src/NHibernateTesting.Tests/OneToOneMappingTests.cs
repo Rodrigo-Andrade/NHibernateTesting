88219a07b1e09b69120975039334c743c1dc4009
Rodrigo Silva de Andrade <dangling@gmail.com>
2013-03-10 15:24:40 -0300

    one to one with foreign key

---
+++ src/NHibernateTesting.Tests/OneToOneMappingTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using FluentAssertions;
+using FluentNHibernate.Mapping;
+using NHibernate;
+using NUnit.Framework;
+
+namespace NHibernateTesting.Tests
+{
+    public class OneToOneWithForeignKeyMappingTests : TestCase
+    {
+        [Test]
+        public void WillNotAllowMoreThenOneAssocioation()
+        {
+            var persisted = new User
+            {
+                Name = "User Name",
+                Address = new Address
+                {
+                    City = "City",
+                    Number = "123",
+                    Street = "Street"
+                }
+            };
+
+            WithNew(session =>
+                {
+                    session.Save(persisted.Address);
+                    session.Save(persisted);
+                });
+
+            var userId = persisted.Id;
+            Action action =
+                () =>
+                WithNew(session =>
+                    {
+                        var retrieved = session.Get<User>(userId);
+                        var newUser = new User {Address = retrieved.Address};
+                        session.Save(newUser);
+                    });
+
+            action.ShouldThrow<HibernateException>();
+        }
+
+        public class User
+        {
+            public virtual int Id { get; protected set; }
+            public virtual string Name { get; set; }
+
+            public virtual Address Address { get; set; }
+        }
+
+        public class Address
+        {
+            public virtual int Id { get; protected set; }
+            public virtual string Street { get; set; }
+            public virtual string Number { get; set; }
+            public virtual string City { get; set; }
+        }
+
+        public class UserMap : ClassMap<User>
+        {
+            public UserMap()
+            {
+                Id(x => x.Id);
+                Map(x => x.Name);
+
+                References(x => x.Address)
+                    .Unique();
+            }
+        }
+
+        public class AddressMap : ClassMap<Address>
+        {
+            public AddressMap()
+            {
+                Id(x => x.Id);
+                Map(x => x.Street);
+                Map(x => x.Number);
+                Map(x => x.City);
+            }
+        }
+    }
+}
